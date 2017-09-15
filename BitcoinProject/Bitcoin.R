8f0cc6824ac46c5ed788a71cbf59c02a72dd2f0c
CaterinaC <caterina.constantinescu@gmail.com>
2017-09-15 16:08:59 +0100

    Updated ggplot2 graphs and made a small dent in time series stuff

--- BitcoinProject/Bitcoin.R
+++ BitcoinProject/Bitcoin.R
@@ -2,7 +2,7 @@
 
 # Libraries and options ---------------------------------------------------
 
-# install.packages( c( "plyr", "data.table", "lubridate", "Rbitcoin", "rbitcoinchartsapi", "igraph", "ggplot2" ) )
+# install.packages( c( "plyr", "data.table", "lubridate", "Rbitcoin", "rbitcoinchartsapi", "igraph", "ggplot2", "tseries" ) )
 
 library(plyr)
 
@@ -15,7 +15,7 @@ library(rbitcoinchartsapi)
 library(igraph)
 library(ggplot2)
 
-
+library(tseries)
 
 # # Choose here:
 # options( scipen = 999 )
@@ -65,17 +65,35 @@ order_book <- Rbitcoin::market.api.process( market = "kraken",
 
 trades[["trades"]][ , tail( .SD, 10 ) ] # get last 10 rows from object
 order_book[[ "asks" ]][ , head( .SD, 10 ) ]
-
+order_book[[ "bids" ]][ , head( .SD, 10 ) ]
 
 Rbitcoin.plot( trades, col = 'blue' )
 Rbitcoin.plot( order_book )
 
 
-
 ggplot( trades$trades, aes( date, price, size = amount ) ) + 
   geom_point() + 
-  geom_smooth( aes( weight = amount ) )
+  geom_smooth( aes( weight = amount ) ) +
+  labs( x = "Time", y = "Price", color = "Symbol", size = "Volume" ) +
+  guides( size = guide_legend( override.aes = list( linetype = 0 ) ) ) +
+  ggtitle( "BTC - EUR" )
 
+
+# Stream of data for trades ( bids and asks ) seem to be independent - no timestamp to match trades on... Not clear to me why.
+# However, to get an idea for what tendencies seem to be occurring for asks, with reference to bids... we can plot them together:
+# Can also add in actual trades - which presumably are bids/asks that managed to converge and actually went through:
+
+trades[["trades"]][ , Source := "Trades" ]
+order_book[[ "asks" ]][ , Source := "Asks" ]
+order_book[[ "bids" ]][ , Source := "Bids" ]
+
+bids_asks_trades <- rbind( trades[["trades"]][ , .SD, .SDcol = c( "price", "Source" ) ],
+                           order_book[[ "asks" ]][ , .SD, .SDcol = c( "price", "Source" ) ], 
+                           order_book[[ "bids" ]][ , .SD, .SDcol = c( "price", "Source" ) ] )
+
+ggplot( bids_asks_trades, aes( x = price, group = Source, fill = Source ) ) + 
+  geom_histogram( color = "black", alpha = 0.4, position = "identity" ) +
+  ggtitle( "(Mis)matches between trade asks, bids and done deals: BTN - EUR")
 
 
 
@@ -97,26 +115,89 @@ ggplot( trades$trades, aes( date, price, size = amount ) ) +
 
 # Source: https://coherentlogic.com/wordpress/middleware-development/r-package-for-the-bitcoincharts-com-api/
 
+??GetMarketData
+??GetHistoricTradeData
+
+
+
 weightedPrices <- rbitcoinchartsapi::GetWeightedPrices()
 weightedPrices$GBP
 
 gbp <- rbitcoinchartsapi::GetMarketData( list( currency = "GBP" ) )
-gbp[[1]]$latest_trade
+# See here for other params: https://bitcoincharts.com/about/markets-api/
+gbp_indices <- sapply( gbp, "[[", "currency" ) == "GBP"
+gbp[ gbp_indices ]
 
-historicTradeData_GBP <- rbitcoinchartsapi::GetHistoricTradeData( list( symbol = "localbtcGBP" ) )
-historicTradeData_USD <- rbitcoinchartsapi::GetHistoricTradeData( list( symbol = "localbtcUSD" ) )
+
+# Getting historic data starting from a specific start date - BUT it does not accept a date further back into the past than 5 days... :
+# Using this, could figure out a day to scan data every 5 days, and add it to models. Or more often, if we want to.
+historicTradeData_GBP <- rbitcoinchartsapi::GetHistoricTradeData( list( symbol = "localbtcGBP", 
+                                                                        start = as.numeric( as.POSIXct( "2017-09-10", 
+                                                                                                        format="%Y-%m-%d" ) ) ) )
+historicTradeData_EUR <- rbitcoinchartsapi::GetHistoricTradeData( list( symbol = "localbtcEUR", 
+                                                                        start = as.numeric( as.POSIXct( "2017-09-10", 
+                                                                                                        format="%Y-%m-%d" ) ) ) )
+historicTradeData_CNY <- rbitcoinchartsapi::GetHistoricTradeData( list( symbol = "btctradeCNY",
+                                                                        start = as.numeric( as.POSIXct( "2017-09-10", 
+                                                                                                        format="%Y-%m-%d" ) ) ) )
 
 setDT( historicTradeData_GBP )[ , symbol := "GBP" ]
-setDT( historicTradeData_USD )[ , symbol := "USD" ]
+setDT( historicTradeData_EUR )[ , symbol := "EUR" ]
+setDT( historicTradeData_CNY )[ , symbol := "CNY" ]
 
 historicTradeData_GBP[  , unixtime := as.POSIXct( unixtime, origin = "1970-01-01" ) ]
-historicTradeData_USD[  , unixtime := as.POSIXct( unixtime, origin = "1970-01-01" ) ]
+historicTradeData_EUR[  , unixtime := as.POSIXct( unixtime, origin = "1970-01-01" ) ]
+historicTradeData_CNY[  , unixtime := as.POSIXct( unixtime, origin = "1970-01-01" ) ]
 
-historicTradeData <- rbind( historicTradeData_GBP, historicTradeData_USD )
+historicTradeData <- rbind( historicTradeData_GBP, historicTradeData_EUR, historicTradeData_CNY )
 
-ggplot( historicTradeData, aes( unixtime, price, size = amount, group = symbol ) ) + 
+ggplot( historicTradeData,
+        aes( unixtime, price, size = amount, group = symbol ) ) +  
   geom_point() + 
-  geom_smooth( aes( weight = amount, color = symbol ) )
+  geom_smooth( aes( weight = amount, group = symbol, color = symbol ) ) + 
+  labs( x = "Time", y = "Price", color = "Symbol", size = "Volume" ) +
+  guides( size = guide_legend( override.aes = list( linetype = 0 ) ) ) +
+  ggtitle( "Bitcoin value over time, and relative to different currencies" )
+
+
+
+# What happened in the last hour?
+nb_hours <- 1
+last_hour_data <- historicTradeData[ unixtime > ( Sys.time() - nb_hours * 3600 ), ]
+
+ggplot( last_hour_data,
+        aes( unixtime, price, size = amount, group = symbol ) ) +  
+  geom_point() + 
+  geom_smooth( aes( weight = amount, group = symbol, color = symbol ) ) + 
+  labs( x = "Time", y = "Price", color = "Symbol", size = "Volume" ) +
+  guides( size = guide_legend( override.aes = list( linetype = 0 ) ) ) +
+  ggtitle( "And now this happened... " )
+
+
+
+
+
+# Time series -------------------------------------------------------------
+
+# Is this time series stationary or non stationary?
+
+# From: http://r-statistics.co/Time-Series-Analysis-With-R.html :
+# Autocorrelation is the correlation of a Time Series with lags of itself. This is a significant metric because it is used commonly to determine if the time series is stationary or not. A stationary time series will have the autocorrelation fall to zero fairly quickly but for a non-stationary series it drops gradually.
+
+acf( historicTradeData_GBP$price )
+acf( historicTradeData_GBP$price, type = "covariance" )
+acf( historicTradeData_GBP$price, type = "correlation" )
+acf( historicTradeData_GBP$price, type = "partial" )
+
+pacf( historicTradeData_GBP$price )
+
+# How to test if a time series is stationary?
+# Use Augmented Dickey-Fuller Test (adf test). A p-Value of less than 0.05 in adf.test() indicates that it is stationary.
+
+adf.test( historicTradeData_GBP$price ) # p-value < 0.05 indicates the TS is stationary
+kpss.test( historicTradeData_GBP$price )
+# Stationary. As shown in the plots above.
+
 
 
 
